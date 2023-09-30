8f26004ba63af8c39ae77f6b1887eb0617c9848f
Matheus Rian <souzamatheusrian@gmail.com>
2023-09-29 23:28:12 -0300

    feat: Criação dos modelos

--- project_cantinho/app_cantinho/models.py
+++ project_cantinho/app_cantinho/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+class Product(models.Model):
+  name = models.CharField(max_length=200)
+  value = models.DecimalField(max_digits=10, decimal_places=2)
+
+class Vendinha(models.Model):
+  name = models.CharField(max_length=200)
+  products = models.ManyToManyField(Product)
+
+class User(models.Model):
+  name = models.CharField(max_length=200)
+  products_cart = models.ManyToManyField(Product)
+
+class Cart(models.Model):
+  user = models.ForeignKey(User, on_delete=models.CASCADE)
+  products = models.ManyToManyField(Product)
+  total = models.DecimalField(max_digits=10, decimal_places=2)
