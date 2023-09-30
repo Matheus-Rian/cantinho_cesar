c2d873b8600bc1b2fb5a8dd5613f6100ea79ecf9
Matheus Rian <souzamatheusrian@gmail.com>
2023-09-30 19:39:56 -0300

    feat: Escolha das vendinhas apartir do RadioSelect

--- project_cantinho/app_cantinho/views.py
+++ project_cantinho/app_cantinho/views.py
@@ -1,14 +1,25 @@
 from django.shortcuts import render
+from .forms import OptionsVendinha
 from .models import VendinhaController
 
 def get_products_by_vendinha(name):
   vendinha = VendinhaController.get_vendinha_by_name(name=name)
-  products = [v for v in vendinha.products.all()]
-  context = {
-    "products": products,
-  }
-  return context
+  products = [product for product in vendinha.products.all()]
+  return products
 
 def index(request):
-  return render(request, "home/index.html", get_products_by_vendinha(name="Apollo"))
+	if request.method == 'POST':
+		form = OptionsVendinha(request.POST)
+		if form.is_valid():
+			opcao = form.cleaned_data['choices']
+		if opcao == 'BRUM':
+				resultado = 'Brum'
+		elif opcao == 'TIRADENTES':
+				resultado = 'Tiradentes'
+		elif opcao == 'APOLLO':
+				resultado = 'Apollo'
+	else:
+		form = OptionsVendinha()
+		resultado = 'Apollo'
 
+	return render(request, "home/index.html", {'form': form, 'products': get_products_by_vendinha(name=resultado)})
