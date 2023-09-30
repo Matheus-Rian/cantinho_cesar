a85d3d88ad025b76de3910c020c278075edd66bd
Talita Fraga <talitafraga@MacBook-Air-de-Talita.local>
2023-09-30 00:28:17 -0300

    modificando admin

--- project_cantinho/app_cantinho/admin.py
+++ project_cantinho/app_cantinho/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Product, Vendinha, User, Cart
 
-# Register your models here.
+admin.site.register(Product)
+admin.site.register(Vendinha)
+admin.site.register(User)
+admin.site.register(Cart)
