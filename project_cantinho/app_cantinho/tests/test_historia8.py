e46b3054aae862c46e5e28938a68745dcde955ff
juliafelixcor <98843736+juliafelixcor@users.noreply.github.com>
2023-11-24 18:26:41 -0300

    ajeitando erro teste historia 8

--- project_cantinho/app_cantinho/tests/test_historia8.py
+++ project_cantinho/app_cantinho/tests/test_historia8.py
@@ -7,7 +7,7 @@ from selenium.webdriver.support import expected_conditions as EC
 import time
 
 options = webdriver.ChromeOptions()
-# options.add_argument("--headless")
+options.add_argument("--headless")
 options.add_argument("--no-sandbox")
 options.add_argument("--disable-dev-shm-usage")
 driver = webdriver.Chrome(options=options)
